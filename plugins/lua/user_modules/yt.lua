6ed3c72ecc5912b27b48053aa14292416f4a461d
Zerf <zerfgog202@gmail.com>
2015-09-20 00:26:20 -0600

    Update yt.lua

--- plugins/lua/user_modules/yt.lua
+++ plugins/lua/user_modules/yt.lua
@@ -1,13 +1,25 @@
 local function random(len) -- credit to swad I guess
+	math.randomseed(os.time() + math.random())
 	local out = ""
+	local r1 = math.random(1, 2)
 	for i = 1, len or 11 do
-		local r = math.random(1, 3)
-		if r == 1 then
-			out = out .. string.char(math.random(97, 122))
-		elseif r == 2 then
-			out = out .. string.char(math.random(65, 90))
+		math.randomseed(os.time() + math.random())
+		if r1 == 1 then -- no russki :(
+			local r2 = math.random(1, 3)
+			if r2 == 1 then
+				out = out .. string.char(math.random(97, 122))
+			elseif r2 == 2 then
+				out = out .. string.char(math.random(65, 90))
+			else
+				out = out .. string.char(math.random(48, 57))
+			end
 		else
-			out = out .. string.char(math.random(48, 57))
+			local r2 = math.random(1, 2)
+			if r2 == 1 then
+				out = out .. utf8.char(math.random(0x430, 0x44F))
+			else
+				out = out .. utf8.char(math.random(0x410, 0x42F))
+			end
 		end
 	end
 	return out
@@ -26,7 +38,7 @@ yt = {}
 local vidbase = "http://youtube.com/watch?v="
 
 function yt.RandomVideo(str, shouldretry, tries)
-	math.randomseed(os.time())
+	math.randomseed(os.time() + math.random())
 	local randomstr = str or random(math.random(3, 24))
 	shouldretry = shouldretry or (not str)
 	tries = tries or 0
